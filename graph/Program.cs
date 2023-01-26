5437dcdbaaba319d30975392ff9b0aaa5300f9b4
Victor Gil <victoredin94@hotmail.com>
2023-01-26 10:31:19 -0500

    update cors domain

--- graph/Program.cs
+++ graph/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
   options.AddDefaultPolicy(
     policy =>
     {
-      policy.WithOrigins("http://localhost:*").AllowAnyMethod().AllowAnyHeader();
+      policy.WithOrigins("http://localhost:5173/").AllowAnyMethod().AllowAnyHeader();
     });
 });
 
